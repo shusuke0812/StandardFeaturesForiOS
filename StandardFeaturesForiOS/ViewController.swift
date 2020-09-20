162a51ab48e874e4d8c7e8d0a717632cda2a23db
shusuke0812 <shu.ota0812@gmail.com>
2020-09-20 21:10:15 +0900

    初期コミット

---
+++ StandardFeaturesForiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
