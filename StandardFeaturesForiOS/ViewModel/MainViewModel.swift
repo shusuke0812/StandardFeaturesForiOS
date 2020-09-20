826ee7f3153c141a49b30ed72986906b1e9d34c0
shusuke0812 <shu.ota0812@gmail.com>
2020-09-21 00:43:52 +0900

    [add]tableViewの状態を管理するviewModelを作成

--- StandardFeaturesForiOS/ViewModel/MainViewModel.swift
+++ StandardFeaturesForiOS/ViewModel/MainViewModel.swift
@@ -5,4 +5,64 @@
 //  Created by Shusuke Ota on 2020/9/20.
 //
 
-import Foundation
+import UIKit
+
+/*
+ * memo:
+ * ログインの状態などのイベントによって、セクション表示を変えることがあるので
+ * viewModelにセクション表示ロジックを持たせてviewへ通知させる
+ */
+
+/// 機能一覧セクション
+enum MainSectionType {
+    /// どのアプリにもありそうなコア機能
+    case core
+    /// 周辺機能
+    case module
+    
+    internal var rows: [MainRowType] {
+        switch self {
+        case .core:
+            return [.login, .advertising, .contact, .terms]
+        case .module:
+            return [.onBording, .push, .carousel, .tapCopy]
+        }
+    }
+}
+
+/// 機能一覧の行
+enum MainRowType: String {
+    case login          = "ログイン"
+    case advertising    = "広告"
+    case contact        = "お問い合わせ"
+    case onBording      = "オンボーディング"
+    case push           = "プッシュ通知"
+    case terms          = "利用規約・プライバシーポリシー"
+    case carousel       = "カルーセル表示"
+    case tapCopy        = "テキスト長押しコピー"
+}
+
+class MainViewModel: NSObject {
+    // tableViewのセクション
+    var sections: [MainSectionType] = []
+}
+
+extension MainViewModel: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sections[section].rows.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sectionType: MainSectionType = self.sections[indexPath.section]
+        switch sectionType {
+        case .core:
+            let cell: MainCoreViewCell = tableView.dequeueReusableCell(withIdentifier: "MainViewCoreCell", for: indexPath) as! MainCoreViewCell
+            return cell
+        case .module:
+            let cell: MainModuleViewCell = tableView.dequeueReusableCell(withIdentifier: "MainModuleViewCell", for: indexPath) as! MainModuleViewCell
+            return cell
+        }
+    }
+}
