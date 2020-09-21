5ace2a25cf95e0b3d0c63358d6e1a829742cbef9
shusuke0812 <shu.ota0812@gmail.com>
2020-09-21 22:57:20 +0900

    [add]viewModel, baseViewとの繋ぎ込み

--- StandardFeaturesForiOS/ViewController/MainViewController.swift
+++ StandardFeaturesForiOS/ViewController/MainViewController.swift
@@ -8,17 +8,55 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    /// BaseView
+    private var baseView: MainBaseView { return self.view as! MainBaseView}
+    /// ViewModel
+    private var viewModel: MainViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = MainViewModel()
+        self.setBaseView()
+    }
+}
+
+extension MainViewController {
+    private func setBaseView() {
+        self.baseView.tableView.tableFooterView = UIView()
+        self.baseView.tableView.delegate = self
+        self.baseView.tableView.dataSource = self.viewModel
     }
 }
 
 extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 0
+        let sectionType: MainSectionType = self.viewModel.sections[indexPath.section]
+        switch sectionType {
+        case .core:
+            return 50
+        case .module:
+            return 50
+        }
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        let sectionRowType: MainRowType = self.viewModel.sections[indexPath.section].rows[indexPath.row]
+        switch sectionRowType {
+        case .login:
+            print("DEBUG： ログインセルがタップされた")
+        case .advertising:
+            print("DEBUG： 広告セルがタップされた")
+        case .contact:
+            print("DEBUG： お問い合わせセルがタップされた")
+        case .onBording:
+            print("DEBUG： オンボーディングセルがタップされた")
+        case .push:
+            print("DEBUG： プッシュ通知セルがタップされた")
+        case .terms:
+            print("DEBUG： 規約セルがタップされた")
+        case .carousel:
+            print("DEBUG： カルーセルセルがタップされた")
+        case .tapCopy:
+            print("DEBUG： 長押しコピーセルがタップされた")
+        }
     }
 }
