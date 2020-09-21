bf84b14c28110558b80233bcc6a5050c8f9ea4c1
shusuke0812 <shu.ota0812@gmail.com>
2020-09-21 22:56:24 +0900

    [add]mainViewのセルを登録する処理を追加

--- StandardFeaturesForiOS/View/BaseView/MainBaseView.swift
+++ StandardFeaturesForiOS/View/BaseView/MainBaseView.swift
@@ -5,4 +5,22 @@
 //  Created by Shusuke Ota on 2020/9/21.
 //
 
-import Foundation
+import UIKit
+
+class MainBaseView: UIView {
+    @IBOutlet weak var tableView: UITableView!
+    override func awakeFromNib() {
+        super .awakeFromNib()
+        self.registerTableViewCell()
+    }
+}
+
+extension MainBaseView {
+    func registerTableViewCell() {
+        let coreNib = UINib(nibName: "MainCoreViewCell", bundle: nil)
+        self.tableView.register(coreNib, forCellReuseIdentifier: "MainCoreViewCell")
+        
+        let moduleNib = UINib(nibName: "MainModuleViewCell", bundle: nil)
+        self.tableView.register(moduleNib, forCellReuseIdentifier: "MainModuleViewCell")
+    }
+}
