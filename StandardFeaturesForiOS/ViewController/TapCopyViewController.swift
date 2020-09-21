90a78c1e1c22da6b5c99196a7fb470dbe7d30dd7
shusuke0812 <shu.ota0812@gmail.com>
2020-09-22 00:17:25 +0900

    [add]長押しタップコピー実装用のviewControllerとstoryboardを実装

---
+++ StandardFeaturesForiOS/ViewController/TapCopyViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TapCopyViewController.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/9/22.
+//
+
+import UIKit
+
+class TapCopyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
