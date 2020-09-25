51ad8cefe6011af42bf6be9d2ceaa041116c288a
shusuke0812 <shu.ota0812@gmail.com>
2020-09-25 11:07:03 +0900

    [add]アクションシートスタイルのアラート表示処理を追加

--- StandardFeaturesForiOS/ViewController/AlertViewController.swift
+++ StandardFeaturesForiOS/ViewController/AlertViewController.swift
@@ -7,7 +7,8 @@
 
 /*
  * このクラスで実装されている主な機能
- *   -
+ *   - ボタンをタップしたらアラートが表示される
+ *   - アラートスタイルは２種類（デフォルト、アクションシート）
  */
 
 import UIKit
@@ -19,12 +20,16 @@ class AlertViewController: UIViewController {
     }
     // MARK: -Action Method
     @IBAction func showAlertButton(_ sender: Any) {
-        self.showAlert()
+        self.showDefaultAlert()
+    }
+    @IBAction func showActionSheetButton(_ sender: Any) {
+        self.showActionSheetAlert()
     }
 }
 
 extension AlertViewController {
-    private func showAlert() {
+    /// デフォルトスタイルのアラート
+    private func showDefaultAlert() {
         // アラートスタイル設定
         let alert: UIAlertController = UIAlertController(title: "タイトル", message: "サブタイトル（メッセージ）", preferredStyle: .alert)
         // OKボタンが押された時の処理
@@ -35,6 +40,26 @@ extension AlertViewController {
         // キャンセルボタンが押された時の処理
         let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: { (action: UIAlertAction!) -> Void in
             // キャンセルボタン押下後に実行
+            print("DEBUG： キャンセルボタンが押されました")
+        })
+        // OKボタンとキャンセルボタンをActionに追加
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        // Alert表示
+        self.present(alert, animated: true, completion: nil)
+    }
+    /// アクションシートのアラート
+    private func showActionSheetAlert() {
+        // アラートスタイル設定
+        let alert: UIAlertController = UIAlertController(title: "タイトル", message: "サブタイトル（メッセージ）", preferredStyle: .actionSheet)
+        // OKボタンが押された時の処理
+        let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) -> Void in
+            // OKボタン押下後の処理
+            print("DEBUG： OKボタンが押されました")
+        })
+        // キャンセルボタンが押された時の処理
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: { (action: UIAlertAction!) -> Void in
+            // キャンセルボタン押下後の処理
             print("DEBUG： キャンセルボタンが押されました")
         })
         // OKボタンとキャンセルボタンをActionに追加
