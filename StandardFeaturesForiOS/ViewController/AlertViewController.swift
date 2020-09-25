0017806d4175ea5e6a135dfb65efbc36bc9da2c3
shusuke0812 <shu.ota0812@gmail.com>
2020-09-25 10:17:49 +0900

    [add]アラート表示処理を追加

--- StandardFeaturesForiOS/ViewController/AlertViewController.swift
+++ StandardFeaturesForiOS/ViewController/AlertViewController.swift
@@ -5,11 +5,42 @@
 //  Created by Shusuke Ota on 2020/9/25.
 //
 
+/*
+ * このクラスで実装されている主な機能
+ *   -
+ */
+
 import UIKit
 
 class AlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    // MARK: -Action Method
+    @IBAction func showAlertButton(_ sender: Any) {
+        self.showAlert()
+    }
+}
+
+extension AlertViewController {
+    private func showAlert() {
+        // アラートスタイル設定
+        let alert: UIAlertController = UIAlertController(title: "タイトル", message: "サブタイトル（メッセージ）", preferredStyle: .alert)
+        // OKボタンが押された時の処理
+        let okAction: UIAlertAction = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) -> Void in
+            // OKボタン押下後に実行
+            print("DEBUG： OKボタンが押されました")
+        })
+        // キャンセルボタンが押された時の処理
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: { (action: UIAlertAction!) -> Void in
+            // キャンセルボタン押下後に実行
+            print("DEBUG： キャンセルボタンが押されました")
+        })
+        // OKボタンとキャンセルボタンをActionに追加
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        // Alert表示
+        self.present(alert, animated: true, completion: nil)
     }
 }
