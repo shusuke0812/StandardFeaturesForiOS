bf64cee9c320521c0588382f62c62cbc38b07a92
shusuke0812 <shu.ota0812@gmail.com>
2020-09-29 00:27:20 +0900

    [add]Signpostsでパフォーマンスを計測するコードを追記

--- StandardFeaturesForiOS/ViewController/PerformanceMeasuringViewController.swift
+++ StandardFeaturesForiOS/ViewController/PerformanceMeasuringViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import os.signpost
 
 class PerformanceMeasuringViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func tapStartPerformanceMesuringButton(_ sender: Any) {
+        self.osLong()
     }
 }
 
@@ -22,5 +26,18 @@ extension PerformanceMeasuringViewController {
         let endDate = Date()
         let time = endDate.timeIntervalSince(beginDate)
         print("DEBUG： \(time)")
+    }
+    /// os.singpostによる処理時間の計測（結果はXcodeのInstrumentsのTimeProfilerで見る）
+    internal func osLong() {
+        let osLog: OSLog = OSLog(subsystem: "jp.shusuke.ota.StandardFeaturesForiOS.StandardFeaturesForiOS", category: "loop")
+        os_signpost(.begin, log: osLog, name: "sample")
+        // 計測したいメソッドを書く
+        self.sample()
+        os_signpost(.end, log: osLog, name: "sample")
+    }
+    private func sample() {
+        for i in 0 ..< 10000000 {
+            print(i)
+        }
     }
 }
