0aeb9079b48650b5b8632536011182b619d20125
shusuke0812 <shu.ota0812@gmail.com>
2020-09-28 09:33:24 +0900

    [add]カルーセル表示に必要なファイルを準備

---
+++ StandardFeaturesForiOS/View/CollectionViewCell/CarouselCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselCollectionViewCell.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/9/28.
+//
+
+import UIKit
+
+class CarouselCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ StandardFeaturesForiOS/ViewController/CarouselViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselViewController.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/9/28.
+//
+
+import UIKit
+
+class CarouselViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
