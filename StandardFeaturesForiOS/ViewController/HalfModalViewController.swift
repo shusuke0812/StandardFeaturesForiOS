5e17325f854d8a3e4a297e92fce28449ce6de9b6
shusuke0812 <shu.ota0812@gmail.com>
2020-10-10 15:57:15 +0900

    [add]ハーフモーダル用のファイルを追加

---
+++ StandardFeaturesForiOS/ViewController/HalfModalViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ModalViewController.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/10/10.
+//
+
+import UIKit
+
+class HalfModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
