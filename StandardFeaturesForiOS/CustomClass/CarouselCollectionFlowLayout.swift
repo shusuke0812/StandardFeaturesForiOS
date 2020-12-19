455f48700914efc2ab79ea47bb976d4da8c8eda4
shusuke0812 <shu.ota0812@gmail.com>
2020-12-19 21:32:21 +0900

    [add]カルーセル表示の画像切り替えスクロールを調整するカスタムClassを追加

---
+++ StandardFeaturesForiOS/CustomClass/CarouselCollectionFlowLayout.swift
@@ -0,0 +1,12 @@
+//
+//  CarouselCollectionFlowLayout.swift
+//  StandardFeaturesForiOS
+//
+//  Created by Shusuke Ota on 2020/12/19.
+//
+
+import UIKit
+
+class CarouselCollectionFlowLayout: UICollectionViewFlowLayout {
+
+}
