534a9845115fbac651068e83faf18c68b5ca4e44
shusuke0812 <shu.ota0812@gmail.com>
2020-12-19 15:04:38 +0900

    [fix]カルーセルViewControllerにViewとViewModelを接続し、セルサイズの設定メソッドも追加

--- StandardFeaturesForiOS/ViewController/CarouselViewController.swift
+++ StandardFeaturesForiOS/ViewController/CarouselViewController.swift
@@ -5,11 +5,39 @@
 //  Created by Shusuke Ota on 2020/9/28.
 //
 
+/*
+ * このクラスで実装されている主な機能
+ *   - 標準カルーセル表示（画像下にボタンのインジケータ有）
+ *   - チラ見せカルーセル（左右の画像を少し見せるカルーセル）
+ */
+
 import UIKit
 
 class CarouselViewController: UIViewController {
-
+    /// BaseView
+    private var baseView: CarouselBaseView { self.view as! CarouselBaseView }
+    /// ViewModel
+    private var viewModel: CarouselViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = CarouselViewModel()
+        self.setDelegateDataSource()
+    }
+}
+// MARK: - Private Method
+extension CarouselViewController {
+    private func setDelegateDataSource() {
+        self.baseView.collectionView.delegate = self
+        self.baseView.collectionView.dataSource = self.viewModel
+    }
+}
+// MARK: - UICollectionView Delegate FlowLayout Method
+extension CarouselViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horizontalSpace: CGFloat = 0.0
+        let cellWidth: CGFloat = self.baseView.bounds.width - horizontalSpace
+        let cellHeight: CGFloat = cellWidth / 2
+        return CGSize(width: cellWidth, height: cellHeight)
     }
 }
