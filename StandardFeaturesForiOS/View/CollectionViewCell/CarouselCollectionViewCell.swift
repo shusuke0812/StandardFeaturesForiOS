46b048aab492a5a06337cc2a7192e8db566daeea
shusuke0812 <shu.ota0812@gmail.com>
2020-12-19 15:34:13 +0900

    [add]カルーセルのView初期化処理を追加

--- StandardFeaturesForiOS/View/CollectionViewCell/CarouselCollectionViewCell.swift
+++ StandardFeaturesForiOS/View/CollectionViewCell/CarouselCollectionViewCell.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class CarouselCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.initUI()
     }
-
+}
+// MARK: -  Setting UI Method
+extension CarouselCollectionViewCell {
+    private func initUI() {
+        self.imageView.backgroundColor = .lightGray
+    }
 }
