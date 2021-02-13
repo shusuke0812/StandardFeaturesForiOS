bf35d82d810ea3ac277b6fa3ef6028168ada3896
shusuke0812 <shu.ota0812@gmail.com>
2021-02-13 13:20:53 +0900

    [add]HealthCare認証設定画面を立ち上げる処理を追加

--- StandardFeaturesForiOS/ViewController/HealthCareViewController.swift
+++ StandardFeaturesForiOS/ViewController/HealthCareViewController.swift
@@ -6,10 +6,34 @@
 //
 
 import UIKit
+import HealthKit
 
 class HealthCareViewController: UIViewController {
-
+    // MARK: - Lifecycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+// MARK: - HealthKit Method
+extension HealthCareViewController {
+    private func setHealthCareAccess() {
+        // 読み込むデータの設定（性別、血液型、歩数）
+        let readData: Set<HKObjectType> = [
+            HKObjectType.characteristicType(forIdentifier: .biologicalSex)!,
+            HKObjectType.characteristicType(forIdentifier: .bloodType)!,
+            HKObjectType.quantityType(forIdentifier: .stepCount)!
+        ]
+        // 書き込めるデータの設定（体重）
+        let writeData: Set<HKSampleType> = [
+            HKSampleType.quantityType(forIdentifier: .bodyMass)!
+        ]
+        let healthStore = HKHealthStore()
+        healthStore.requestAuthorization(toShare: writeData, read: readData) { status, error in
+            if status {
+                print("DEBUG: 認証済み")
+            } else {
+                print("DEBUG: 認証拒否")
+            }
+        }
     }
 }
