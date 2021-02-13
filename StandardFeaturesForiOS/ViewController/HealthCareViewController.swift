0af7ebc75605fb5e6179e8df31184b82e888be82
shusuke0812 <shu.ota0812@gmail.com>
2021-02-13 21:19:04 +0900

    [add/fix]ヘルスケア画面の下記内容を変更
    ・性別、血液型データを読み込んで表示させるようにした
    ・ヘルスケアアプリに許可申請を出した後はボタンをタップしたらその旨をアラート表示するようにした

--- StandardFeaturesForiOS/ViewController/HealthCareViewController.swift
+++ StandardFeaturesForiOS/ViewController/HealthCareViewController.swift
@@ -17,22 +17,31 @@ class HealthCareViewController: UIViewController {
     @IBOutlet weak var bodyMassLabel: UILabel!
     /// HealthStore
     private let healthStore = HKHealthStore()
+    /// HealthCare Authフラグ
+    private var isHealthCareRequestAuthorization = false
     
     // MARK: - Lifecycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
         self.initUI()
+        self.updatedHealthCareData()
     }
     // MARK: - Action Method
     @IBAction func didTapHealthCareConnectionButton(_ sender: Any) {
-        UIAlertController.showAlertView(vc: self, title: "ヘルスケアアプリと\n連携しますか", message: nil, preferredStyle: .alert, okBtnLabel: "OK", cancelBtnLabel: "キャンセル") {
-            self.setHealthCareAccess()
+        if !isHealthCareRequestAuthorization {
+            UIAlertController.showAlertView(vc: self, title: "ヘルスケアアプリと\n連携しますか", message: nil, preferredStyle: .alert, okBtnLabel: "OK", cancelBtnLabel: "キャンセル") {
+                self.setHealthCareAccess { status in
+                    if status { self.updatedHealthCareData() }
+                }
+            }
+        } else {
+            UIAlertController.showAlertView(vc: self, title: "認証リクエス済みです\n変更は設定画面から！", message: nil, preferredStyle: .alert, okBtnLabel: "OK", cancelBtnLabel: nil, completion: nil)
         }
     }
 }
 // MARK: - HealthKit Method
 extension HealthCareViewController {
-    private func setHealthCareAccess() {
+    private func setHealthCareAccess(completion: @escaping (Bool) -> Void) {
         // 読み込むデータの設定（性別、血液型、歩数）
         let readData: Set<HKObjectType> = [
             HKObjectType.characteristicType(forIdentifier: .biologicalSex)!,
@@ -46,13 +55,63 @@ extension HealthCareViewController {
         self.healthStore.requestAuthorization(toShare: writeData, read: readData) { status, error in
             if status {
                 print("DEBUG: 認証済み")
+                self.isHealthCareRequestAuthorization = true
+                completion(status)
             } else {
                 print("DEBUG: 認証拒否")
+                completion(status)
             }
         }
     }
-    private func readHealthCareData() {
-        
+    private func updatedHealthCareData() {
+        self.readBloodType()
+        self.readBiologicalSex()
+    }
+    /// 性別を読み込む
+    private func readBiologicalSex() {
+        var biologicalSex: String?
+        do {
+            let biologicalSextInfo = try self.healthStore.biologicalSex()
+            switch biologicalSextInfo.biologicalSex {
+            case .male:     biologicalSex = "男性"
+            case .female:   biologicalSex = "女性"
+            case .notSet:   biologicalSex = "未設定"
+            case .other:    biologicalSex = "他"
+            default:        print("DEBUG: 性別読み込みエラー")
+            }
+            if let biologicalSex = biologicalSex {
+                DispatchQueue.main.async {
+                    self.biologicalSexLabel.text = biologicalSex
+                }
+            }
+        } catch {
+            print("DEBUG: \(error.localizedDescription)")
+        }
+    }
+    /// 血液型を読み込む
+    private func readBloodType() {
+        var bloodType: String?
+        do {
+            let bloodTypeInfo = try self.healthStore.bloodType()
+            switch bloodTypeInfo.bloodType {
+            case .aPositive:    bloodType = "A+"
+            case .aNegative:    bloodType = "A-"
+            case .bPositive:    bloodType = "B+"
+            case .bNegative:    bloodType = "B-"
+            case .abPositive:   bloodType = "AB+"
+            case .abNegative:   bloodType = "AB-"
+            case .oPositive:    bloodType = "O+"
+            case .oNegative:    bloodType = "O-"
+            default:            print("DEBUG: 血液型読み込みエラー")
+            }
+            if let bloodType = bloodType {
+                DispatchQueue.main.async {
+                    self.bloodTypeLabel.text = bloodType
+                }
+            }
+        } catch {
+            print("DEBUG: \(error.localizedDescription)")
+        }
     }
 }
 // MARK: - Setting UI Method
